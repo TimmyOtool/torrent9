73bcb54fa1fc0d9edaae89d8c1da67c2f9f752f3
root <melinm@gmail.com>
2017-01-31 09:42:21 +0100

    indent

--- main.py
+++ main.py
@@ -45,67 +45,65 @@ class torrent9(TorrentProvider, MovieProvider):
                 torrent_rows = html.findAll('tr')
                  
                 for result in torrent_rows:
-        		try:
-                    if not result.find('a'):
-						continue
-
-						title = result.find('a').get_text(strip=False)
-                    log.info('found title %s',title)
-
-					testname=namer_check.correctName(title.lower(),movie)
-					if testname==0:
-						log.info('%s not match %s',(title.lower(),movie['info']['titles']))
-						continue
-                    log.info('title %s match',title)
-
-                    tmp = result.find("a")['href'].split('/')[-1].replace('.html', '.torrent').strip()
-                    download_url = (self.urls['site'] + 'get_torrent/{0}'.format(tmp) + ".torrent")
-                    detail_url = (self.urls['site'] + 'torrent/{0}'.format(tmp))
-			        log.debug('download_url %s',download_url)	
-
-                    if not all([title, download_url]):
-        				continue
-
-					seeders = int(result.find(class_="seed_ok").get_text(strip=True))
-                    leechers = int(result.find_all('td')[3].get_text(strip=True))
+                    try:
+                        if not result.find('a'):
+                            continue
                     
-			        size = result.find_all('td')[1].get_text(strip=True)
-                    
-					def extra_check(item):
-						return True
-                    
-					size = size.lower()
-                    size = size.replace("go", "gb")
-                    size = size.replace("mo", "mb")
-                    size = size.replace("ko", "kb")
-                    size=size.replace(' ','')
-                    size=self.parseSize(str(size))
-			        
-					new={}		
-					new['id'] = id
-					new['name'] = title.strip()
-					new['url'] = download_url
-					new['detail_url'] = detail_url
-					new['size'] = size
-					new['seeders'] = seeders
-					new['leechers'] = leechers
-                    new['extra_check'] = extra_check
-					new['download'] = self.loginDownload  
-                    results.append(new)
-                    log.info(results)
-					id = id+1
-				except StandardError, e:
-                    log.info('boum %s',e)
+                        title = result.find('a').get_text(strip=False)
+                        log.info('found title %s',title)
+
+                        testname=namer_check.correctName(title.lower(),movie)
+                        if testname==0:
+	                    log.info('%s not match %s',(title.lower(),movie['info']['titles']))
 		            continue
-          
+                        log.info('title %s match',title)
+
+                        tmp = result.find("a")['href'].split('/')[-1].replace('.html', '.torrent').strip()
+                        download_url = (self.urls['site'] + 'get_torrent/{0}'.format(tmp) + ".torrent")
+                        detail_url = (self.urls['site'] + 'torrent/{0}'.format(tmp))
+	                log.debug('download_url %s',download_url)	
+
+                        if not all([title, download_url]):
+                            continue
+
+                        seeders = int(result.find(class_="seed_ok").get_text(strip=True))
+                        leechers = int(result.find_all('td')[3].get_text(strip=True))
+	                size = result.find_all('td')[1].get_text(strip=True)
+                    
+                        def extra_check(item):
+	                    return True
+                    
+	                size = size.lower()
+                        size = size.replace("go", "gb")
+                        size = size.replace("mo", "mb")
+                        size = size.replace("ko", "kb")
+                        size=size.replace(' ','')
+                        size=self.parseSize(str(size))
+			        
+                        new={}		
+		        new['id'] = id
+		        new['name'] = title.strip()
+		        new['url'] = download_url
+		        new['detail_url'] = detail_url
+		        new['size'] = size
+		        new['seeders'] = seeders
+		        new['leechers'] = leechers
+                        new['extra_check'] = extra_check
+		        new['download'] = self.loginDownload  
+                        results.append(new)
+                        log.info(results)
+		        id = id+1
+		    except StandardError, e:
+                        log.info('boum %s',e)
+		        continue
             except AttributeError:
                 log.debug('No search results found.')
         else:
             log.debug('No search results found.')
 
     def login(self):
-		log.info('Try to login on torrent9')
-		return True
+	log.info('Try to login on torrent9')
+	return True
         
     def download(self, url = '', nzb_id = ''):
         log.debug('download %s',url) 
